25c7f55490a72959af2f7584a0a3ae5d8d92cd00
lakshyamahajan <105827166+lakshyamahajan@users.noreply.github.com>
2022-08-17 21:34:32 -0700

    Lab 5 Screenshots and scripts

---
+++ powershell/lab5/module_ps.psm1
@@ -0,0 +1,80 @@
+
+new-item -path alias:np -value notepadc| out-null
+
+function welcome {
+write-output "Welcome to planet $env:computername Overlord $env:username"
+$now = get-date -format 'HH:MM tt on dddd'
+write-output "It is $now."
+}
+
+function get-cpuinfo
+{
+  Get-CimInstance CIM_Processor | select manufacturer, CurrentClockSpeed, MaxClockSpeed, NumberOfCores
+}
+
+function get-mydisks 
+{
+    Get-Disk | select Manufacturer, Model, SerialNumber, FirmwareVersion, Size
+}
+
+
+function HardwareInfo {
+get-wmiobject -class win32_computersystem         
+}
+
+function OSInfo {
+Get-wmiobject -class win32_operatingsystem | Select-Object Name, Version | Format-List
+}
+
+function ProcessorInfo {
+Get-WmiObject -class win32_processor | Select-Object Description, Maxclockspeed, Currentclockspeed,L1CacheSize,L2CacheSize,L3CacheSize | Format-List
+}
+
+function PhysicalMemoryInfo {
+$physicalMemory = Get-WmiObject -class win32_physicalmemory
+
+foreach($memory in $physicalMemory) {
+ New-Object -TypeName psobject -Property @{
+	 Vendor = $memory.manufacturer
+	 Description = $memory.description
+	 Size = $memory.capacity/1mb
+	 Bank = $memory.banklabel
+	 Slot = $memory.devicelocator
+ } | ft -Auto  Vendor,Description,Size, Bank, Slot
+ 
+ $totalcapacity += $memory.capacity/1mb
+} 
+"Total RAM: ${totalcapacity}MB " 
+}
+
+Function DiskInfo {
+$diskdrives = Get-CIMInstance CIM_diskdrive
+
+  foreach ($disk in $diskdrives) {
+      $partitions = $disk|get-cimassociatedinstance -resultclassname CIM_diskpartition
+      foreach ($partition in $partitions) {
+            $logicaldisks = $partition | get-cimassociatedinstance -resultclassname CIM_logicaldisk
+            foreach ($logicaldisk in $logicaldisks) {
+                     new-object -typename psobject -property @{
+															   Vendor = $disk.Manufacturer
+                                                               Model=$disk.Model
+                                                               Size=$logicaldisk.Size
+                                                               Freespace=$logicaldisk.FreeSpace
+                                                               PercentageFree=($logicaldisk.Freespace/$logicaldisk.Size)*100
+                                                               }  | Format-Table -auto  Vendor, Model, Size, Freespace, PercentageFree
+                                                               
+           } 
+           
+      } 
+  }  
+ 
+
+} 
+
+Function NetworkInfo {
+Get-CimInstance win32_networkadapterconfiguration | Where-Object IPEnabled | Select-Object Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder | Format-Table
+}
+
+Function VideoInfo {
+Get-WmiObject -Class Win32_VideoController | Select-Object -Property Description,@{Name="CurrentScreenResolution";Expression={ $_.CurrentHorizontalResolution.ToString() + " X " + $_.CurrentVerticalResolution.ToString() }} | Format-List
+}
---
+++ powershell/lab5/psLab5.ps1
@@ -0,0 +1,27 @@
+﻿param ( [switch]$System, [switch]$Disks, [switch]$Network)
+
+if ($System) {   
+HardwareInfo  
+OSInfo
+ProcessorInfo
+PhysicalMemoryInfo
+VideoInfo
+}
+elseif ($Disks) { 
+DiskInfo
+}
+elseif ($Network) { 
+NetworkInfo
+}
+
+else {
+
+HardwareInfo
+OSInfo
+ProcessorInfo
+PhysicalMemoryInfo
+DiskInfo
+NetworkInfo
+VideoInfo
+
+}
